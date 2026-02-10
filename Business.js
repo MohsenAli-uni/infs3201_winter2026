c7bd215e52c2f0a9ea6612ba4c61e84b43e222c3
MohsenAli-uni <60305864@udst.edu.qa>
2026-02-10 22:27:47 +0300

    function 3 (assignShift) created in the business layer

--- Business.js
+++ Business.js
@@ -28,3 +28,42 @@ async function addEmployee(name, phone) {
     return "Employee added..."
 }
 
+async function assignShift(employeeID,shiftID) {
+    let employees = await persistence.readEmployeesData()
+    let shifts = await persistence.readShiftsData()
+    let assignments = await persistence.readAssignmentsData()
+
+    let employeeFound = false
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].employeeId === employeeID) {
+            employeeFound = true
+        }
+    }
+    if (employeeFound === false) {
+        
+        return "Employee does not exist"
+    }
+
+    let shiftFound = false
+    for (let i = 0; i < shifts.length; i++) {
+        if (shifts[i].shiftId === shiftID) {
+            shiftFound = true
+        }
+    }
+    if (shiftFound === false) {
+        
+        return "Shift does not exist"
+    }
+
+    for (let i = 0; i < assignments.length; i++) {
+        if (assignments[i].employeeId === employeeID && assignments[i].shiftId === shiftID) {
+            
+            return "Employee already assigned to shift"
+        }
+    }
+
+    await persistence.assignShift(employeeID, shiftID)
+
+    return "Shift Recorded"
+    
+}
