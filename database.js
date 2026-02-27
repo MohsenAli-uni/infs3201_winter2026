666f0e3da656b12649cccd5616b1beacbbadb390
MohsenAli-uni <60305864@udst.edu.qa>
2026-02-28 01:59:41 +0300

    creation database.js for connection mongodb

---
+++ database.js
@@ -0,0 +1,11 @@
+const {MongoClient} = require("mongodb");
+const uri="mongodb+srv://60305864:55482521m@cluster0.bw3r7xd.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri);
+
+
+async function getDb(){
+    await client.connect();
+    return client.db("infs3201_winter2026");
+}
+
+module.export={getDb};
